6f42a6bf9d87789f1bf52f67df142f5296f1d72c
Matheus Davi Félix Migliani França <128201513+MatheusMigliani@users.noreply.github.com>
2024-04-28 04:23:39 -0300

    api deals working
    api deals page working

--- src/pages/Inicial.tsx
+++ src/pages/Inicial.tsx
@@ -1,28 +1,50 @@
+import React, { useEffect, useState } from "react";
 import {
-  IonButton,
+  IonCard,
   IonContent,
   IonHeader,
-  IonMenuButton,
   IonPage,
-  IonTitle,
+  IonText,
   IonToolbar,
+  IonTitle
 } from "@ionic/react";
-import { homeOutline, cogOutline } from "ionicons/icons";
-import React from "react";
 import Hamburguerbotao from "../components/hamburguerbotao";
+import { getDeals } from "../services/api";
 
 const Inicial: React.FC = () => {
-  const paths = [
-    { name: "Home", url: "/menu/Inicial", icon: homeOutline },
-    { name: "Settings", url: "/menu/settings", icon: cogOutline },
-  ];
+  const [deals, setDeals] = useState([]);
+
+  useEffect(() => {
+    const fetchDeals = async () => {
+      const data = await getDeals();
+      console.log("Fetched data:", data);
+      if (data) {
+        setDeals(data);
+      }
+    };
+
+    fetchDeals();
+  }, []);
 
   return (
     <IonPage>
       <IonHeader>
-        <Hamburguerbotao/>
+        <IonToolbar>
+          <Hamburguerbotao />
+        </IonToolbar>
       </IonHeader>
-      <IonContent color={"dark"} className="ion-padding">UI goes here...</IonContent>
+      <IonContent color={"dark"} className="ion-padding">
+        {deals.map((deal) => (
+          <IonCard key={deal.id} color={"success"} className="ion-padding">
+            <IonText color={'dark'}>
+              <h2>{deal.title}</h2>
+              <p>{deal.deal.historyLow.amount}</p>
+              <p>Price: ${deal.deal.price.amount} (Regular: ${deal.deal.regular.amount})</p>
+              <p>Store: {deal.deal.shop.name}</p>
+            </IonText>
+          </IonCard>
+        ))}
+      </IonContent>
     </IonPage>
   );
 };
---
+++ src/services/api.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const apiKey = "ecc3f1d88691c5c5fc3010084cf9973dd7945d79";
+const http = axios.create({
+  baseURL: "https://api.isthereanydeal.com/",
+});
+
+// Função para obter dados, com a chave de API como parâmetro
+export const getDeals = async () => {
+  try {
+    const response = await http.get("deals/v2", { params: { key: apiKey } });
+    console.log("API Data:", response.data);
+    // Acessar a lista de deals diretamente da propriedade 'list' do objeto
+    return response.data.list; // Assumindo que a estrutura da resposta está correta
+  } catch (error) {
+    console.error("Error fetching deals:", error);
+  }
+};
